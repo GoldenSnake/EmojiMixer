5e948d1e2f597d05da8b720c8e9d6e177f447b2f
GoldenSnake <elst1995@gmail.com>
2024-11-11 15:57:16 +0300

    Добавлена UICollectionView

--- EmojiMixer/EmojiMixerVC.swift
+++ EmojiMixer/EmojiMixerVC.swift
@@ -6,13 +6,36 @@ class EmojiMixerVC: UIViewController {
     private let undoButton = UIBarButtonItem()
     private let addButton = UIBarButtonItem()
     
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    let emojies = [
+        // Улыбки и лица
+        "😀", "😂", "😊", "😍", "😜",
+        // Жесты и руки
+        "👍", "👎", "👌", "👏", "🙏",
+        // Люди и деятельность
+        "💃", "🕺", "👫", "👩‍❤️‍👨", "👨‍👩‍👧",
+        // Животные и природа
+        "🐶", "🐱", "🐰", "🦁", "🐼",
+        // Еда и напитки
+        "🍏", "🍕", "🍔", "🍣", "🍫",
+        // Путешествия и транспорт
+        "✈️", "🚗", "🚀", "🚁", "🚲",
+        // Символы и объекты
+        "💡", "📱", "💻", "⌛️", "🔒"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
+        view.addSubview(collectionView)
         
         setupNavigationBar()
+        setupCollectionView()
+        
     }
+    
+    
     // MARK: - NavigationBar
     
     private func setupNavigationBar() {
@@ -40,6 +63,30 @@ class EmojiMixerVC: UIViewController {
         navigationItem.rightBarButtonItem = undoButton
     }
     
+    // MARK: - CollectionView
+    
+    func setupCollectionView() {
+        
+        //register
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        //DataSourse
+        collectionView.dataSource = self
+        
+        setupCollectionConstraints()
+    }
+    
+    func setupCollectionConstraints() {
+            // Устанавливаем авто-лайаут
+            collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
+        }
     
     // MARK: - @objc
     @objc private func addButtonDidTap() {
@@ -52,3 +99,20 @@ class EmojiMixerVC: UIViewController {
     
 }
 
+
+// MARK: - UICollectionViewDataSource
+
+extension EmojiMixerVC: UICollectionViewDataSource {
+    
+    //кол-во элементов
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojies.count
+    }
+    
+    //настройка ячейки
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .lightGray // Цвет фона для ячейки
+        return cell
+    }
+}
